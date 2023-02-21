799a413506b0e33c80cbb73c9a1057379fb69efe
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-02-21 13:39:53 +0000

    updated app and tests file after initial work on ticket 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ afterAll(() => {
   return db.end();
 });
 
-describe.only("app", () => {
+describe("app", () => {
   describe("/api/topics", () => {
     it("200: GET - Responds with an array of topic objects. each object should have a slug and description properties", () => {
       return request(app)
@@ -28,6 +28,44 @@ describe.only("app", () => {
               description: expect.any(String),
             });
           });
+        });
+    });
+  });
+});
+
+describe("app", () => {
+  describe("/api/articles", () => {
+    it("200: GET - Responds with an array of article objects. each object should have the following properties: author, title, article_id, topic, created_at, votes, article_img_url, comment_count", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then((response) => {
+          ({ articles } = response.body);
+          expect(articles).toHaveLength(12);
+          articles.forEach((article) => {
+            expect(typeof article).toBe("object");
+            expect(article).toHaveProperty("comment_count");
+            expect(article).toMatchObject({
+              article_id: expect.any(Number),
+              title: expect.any(String),
+              topic: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(String),
+            });
+          });
+        });
+    });
+    it("200: GET - returns the articles sorted by date in descending order", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then((response) => {
+          ({ articles } = response.body);
+          expect(articles).toBeSortedBy("created_at", { descending: true });
         });
     });
   });
